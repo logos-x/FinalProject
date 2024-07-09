8cd16a3bcb6447eceab0c05d5bf5d0977dd318bb
Tran Quang Tai <95439174+logos-x@users.noreply.github.com>
2024-07-09 15:59:45 +0700

    Delete Library directory

--- Library/src/main/java/com/example/ecommercelaptopshop/library/repository/ConfigurationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommercelaptopshop.library.repository;
-
-import com.example.ecommercelaptopshop.library.entity.Configuration;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConfigurationRepository extends JpaRepository<Configuration, Long> {
-}
--- Library/src/main/java/com/example/ecommercelaptopshop/library/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommercelaptopshop.library.repository;
-
-import com.example.ecommercelaptopshop.library.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
